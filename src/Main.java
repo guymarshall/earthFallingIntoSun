3793d0366be2e9c32c1be58c24834e82e540e619
Guy Marshall <guy@cslgroupservices.co.uk>
2022-08-19 12:30:35 +0100

    Add large proportion of calculating logic

--- src/Main.java
+++ src/Main.java
@@ -20,10 +20,28 @@ public class Main {
         And similar to work out the new v.
         */
 
-        final double GRAVITATIONAL_CONSTANT = 0.000000000066743d;
-        final double MASS_OF_SUN_KG = 1988470000000000000000000000000d;
-        final double DISTANCE_BETWEEN_EARTH_AND_SUN_M = 151480000000d;
+        final double GRAVITATIONAL_CONSTANT = 0.000000000066743;
+        final double MASS_OF_SUN_KG = 1988470000000000000000000000000.0;
+        final double DISTANCE_BETWEEN_EARTH_AND_SUN_M = 151480000000.0;
 
         int deltaT = 1;
+        int time = 0;
+        double distanceFromSun = DISTANCE_BETWEEN_EARTH_AND_SUN_M;
+        double acceleration = 0.0;
+        double velocity = 0.0;
+        double deltaV = 0.0;
+        double deltaS = 0.0;
+
+        while (distanceFromSun > 0) {
+            acceleration = Functions.calculateAcceleration(GRAVITATIONAL_CONSTANT, MASS_OF_SUN_KG, distanceFromSun);
+            deltaV = acceleration * deltaT;
+            velocity += deltaV;
+            deltaS = velocity * deltaT;
+            distanceFromSun += deltaS;
+
+            System.out.println("");
+
+            time += deltaT;
+        }
     }
 }

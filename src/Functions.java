68032f44c40ba23cf4c62012262c52318db5b5d0
Guy Marshall <guymarshall9@gmail.com>
2022-08-20 14:18:11 +0100

    Remove unnecessary Functions.java

--- src/Functions.java
+++
@@ -1,5 +0,0 @@
-public class Functions {
-    public static double calculateAcceleration(double gravitationalConstant, double mass, double radius) {
-        return -1 * (gravitationalConstant * mass) / (radius * radius);
-    }
-}
